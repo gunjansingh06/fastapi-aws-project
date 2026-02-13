3ffeb25908b3ac43d3ad1bba029da13913671e69
gunjansingh06 <gunjan001singh@gmail.com>
2026-02-13 17:25:11 +0530

    Feat:update gitignore file

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.database import engine
 app = FastAPI()
 
 @app.on_event("startup")
-async def startup():
+async def startup():   
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
